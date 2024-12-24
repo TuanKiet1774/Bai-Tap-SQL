16d16c66a7ed28b262168b30f0924af48b083ed3
TuanKiet1774 <167632480+TuanKiet1774@users.noreply.github.com>
2024-12-25 02:08:48 +0700

    commit

---
+++ Ôn GK QTCSDL/QL_RapPhim.sql
@@ -0,0 +1,131 @@
+﻿create database RapChieuPhim;
+use RapChieuPhim;
+
+create table Rap(
+MaRap varchar(10) constraint pk_rap primary key,
+TenRap nvarchar(30),
+Diachi nvarchar(50),
+TongSoGhe int,
+)
+
+create table Phim(
+MaPhim varchar(10) constraint pk_phim primary key,
+TenPhim nvarchar(30),
+NamSanXuat int,
+HangSanXuat nvarchar(30)
+)
+
+create table XuatChieu(
+MaXuatChieu varchar(5) constraint pk_xuatchieu primary key,
+TenXuat nvarchar(10),
+TuGio time,
+GiaVe money
+)
+
+create table BanVe(
+Ngay datetime,
+MaRap varchar(10) constraint fk_bv_r foreign key (MaRap) references Rap(MaRap),
+MaXuatChieu varchar(5)constraint fk_bv_xc foreign key (MaXuatChieu) references XuatChieu(MaXuatChieu),
+MaPhim varchar(10) constraint fk_bv_p foreign key (MaPhim) references Phim(MaPhim),
+SoVeBan int
+constraint pk_bv primary key (Ngay, MaRap, MaXuatChieu),
+)
+
+insert into Rap values
+('MN',N'Rap Mini',N'21 Hoàng Hoa Thám',200),
+('TT',N'Rap Tân Tiến',N'8 Lê Thành Phương', 300),
+('CGV',N'Rap CGV Nha Trang',N'754 Vĩnh Hiệp, Nha Trang',400),
+('LTN',N'Rap Lote Thái Nguyên', N'60 Thái Nguyên, Phước Tân',500),
+('LTP',N'Rap lote Trần Phú', N'78 Trần Phú, P.Lộc Thọ', 500)
+
+insert into Phim values 
+('P001',N'Lời Nguyền Huyết Ngải',2021,N'Giải Phóng'),
+('P002',N'King Kong',2020,N'Universal'),
+('P003',N'Avengers: End Game',2019,N'Marvel Studios'),
+('P004',N'Bố Già',2021,N'Trấn Thành Town HKFilm'),
+('P005',N'Nghê Siêu Dễ',2023,N'Thu Trang Entertainment')
+
+select * from Phim
+insert into XuatChieu values
+('S1',N'Sáng','09:00',30.000),
+('S2',N'Sáng','09:30',35.000),
+('C1',N'Chiều','14:00',45.000),
+('C2',N'Chiều','16:30',45.000),
+('T1',N'Tối','18:45',55.000),
+('T2',N'Tối','20:00',50.000)
+
+insert into BanVe values
+('2023-07-17','CGV','C2','P003',30),
+('2023-12-17','LTN','S1','P004',50),
+('2023-04-02','LTP','T1','P002',40),
+('2023-10-17','TT','C1','P001',20),
+('2023-10-30','TT','T2','P005',60)
+
+
+--Câu 2
+--a. Phim đạt doanh thu cao nhất trong tháng 10/2023 (Mã phim, tên phim, hãng sản xuất, doanh thu)
+select top 1 with ties  p.MaPhim as 'Mã phim', p.TenPhim as 'Tên phim', p.HangSanXuat as 'Hãng sản xuất',
+sum(bv.SoVeBan * xc.GiaVe) as 'Doanh thu'
+from Phim p inner join BanVe bv on bv.MaPhim = p.MaPhim
+inner join XuatChieu xc on xc.MaXuatChieu = bv.MaXuatChieu
+where month(bv.Ngay) = 10 and year(bv.Ngay) = 2023
+group by p.MaPhim, p.TenPhim, p.HangSanXuat
+order by 'Doanh thu' desc
+
+--b. Tính số vé bán được theo từng phim của từng rap tháng 12/2023
+select p.TenPhim as'Phim', r.TenRap as 'Rạp' ,sum(bv.SoVeBan) as'Số vé đã bán' 
+from  Phim p inner join BanVe bv on p.MaPhim = bv.MaPhim 
+inner join Rap r on r.MaRap = bv.MaRap
+where month(bv.Ngay) = 12 and year(bv.Ngay) = 2023
+group by p.TenPhim,r.TenRap
+
+--c. Liệt kê các phim có chiếu nhưng không có khán giả nào xem (Mã phim, tên phim, hãng sản xuất, năm sản xuất)
+select p.MaPhim as 'Mã phim', p.TenPhim as 'Tên phim', p.HangSanXuat as 'Hãng SX', p.NamSanXuat as 'Năm' 
+from Phim p inner join BanVe bv on bv.MaPhim = p.MaPhim
+where bv.SoVeBan is null
+
+--d. Bổ sung cho các rap có số ghế dưới 30, mỗi rap thêm 20 ghế
+update Rap set TongSoGhe = TongSoGhe + 20 where TongSoGhe < 30
+
+--e. Cho biết 2 phim có đông khán giả nhất năm 2023 (Mã phim, tên phim, số khán giả)
+select top 2 with ties p.MaPhim as 'Mã', p.TenPhim as 'Phim', sum(bv.SoVeBan) as 'Số khán giả' 
+from Phim p inner join BanVe bv on bv.MaPhim = p.MaPhim
+where year(bv.Ngay) = 2023
+group by p.MaPhim, p.TenPhim
+order by sum(bv.SoVeBan) desc
+
+--f. Liệt kê tên phim chiếu trong tất cả các rạp
+select r.TenRap as 'Rạp phim', p.TenPhim as 'Phim' from Phim p inner join BanVe bv on bv.MaPhim = p.MaPhim
+inner join Rap r on r.MaRap = bv.MaRap
+
+--Câu 3
+--a. Tạo thủ tục cho phép chèn dữ liệu vào bảng BanVe
+create proc Chen_BanVe
+@Ngay datetime,
+@MaRap varchar(10),
+@MaXuatChieu varchar(5),
+@MaPhim varchar(10),
+@SoVeBan int
+as
+begin insert into BanVe (Ngay, MaRap, MaXuatChieu, MaPhim, SoVeBan)
+values (@Ngay, @MaRap, @MaXuatChieu, @MaPhim, @SoVeBan) end
+
+--Vd minh họa
+exec Chen_BanVe '2023-03-25', 'LTN', 'S1', 'P002', 100;
+
+--b. Tạo hàm trả về một bảng, liệt kê thông tin các rạp có chiếu phim 'Lời Nguyền Huyết Ngãi' 
+--(Tên rạp, Địa chỉ, ngày chiếu, xuất chiếu, Tổng số vé bán được )
+create function LietKeThongTin () 
+returns table
+as return (
+select r.TenRap as 'Rạp', r.DiaChi as 'Địa chỉ', bv.Ngay as 'Ngày chiếu', xc.TenXuat as 'Xuất chiếu',
+sum(bv.SoVeBan) as 'Tổng số vé'
+from Rap r inner join BanVe bv on bv.MaRap = r.MaRap
+inner join Phim p on p.MaPhim = bv.MaPhim
+inner join XuatChieu xc on xc.MaXuatChieu = bv.MaXuatChieu
+where p.TenPhim = N'Lời Nguyền Huyết Ngải'
+group by r.TenRap, r.DiaChi, bv.Ngay, xc.TenXuat
+)
+
+--Vd minh họa
+select * from LietKeThongTin()
---
+++ Ôn GK QTCSDL/QL_Sach.sql
@@ -0,0 +1,157 @@
+﻿create database QL_Sach;
+use QL_Sach;
+
+create table NguoiMuon(
+MSNM varchar(5) constraint pk_msnm primary key,
+HoLotNM nvarchar(30),
+TenNM nvarchar(10),
+Doi smallint,
+Thon nvarchar(50)
+)
+
+create table PhanLoai(
+MPL varchar(10) constraint pk_pl primary key,
+ThongTinPL nvarchar(50),
+)
+
+create table NXB(
+MSNXB varchar(5) constraint pk_nxb primary key,
+TenNXB nvarchar(50),
+)
+
+create table Sach(
+MSSACH varchar(10) constraint pk_mss primary key,
+TenSach nvarchar(50),
+MPL varchar(10) constraint fk_s_pl foreign key (MPL) references PhanLoai(MPL),
+MSNXB varchar(5) constraint fk_s_nxb foreign key (MSNXB) references NXB(MSNXB),
+)
+
+create table ChiTietMS(
+MSNM varchar(5) constraint fk_ctms_nm foreign key (MSNM) references NguoiMuon(MSNM),
+MSSACH varchar(10) constraint fk_ctms_mss foreign key (MSSACH) references Sach(MSSACH),
+NgayMuon date,
+NgayTra date null,
+constraint pk_ctms primary key (MSNM,MSSACH,NgayMuon)
+)
+
+insert into NguoiMuon values
+('TN001',N'Nguyễn Văn',N'Mít',01,N'Thôn Đoài'),
+('TN002',N'Lê Thị Mỹ',N'Xoài',02,N'Thôn Đông'),
+('TN003',N'Võ Thị',N'Cam',03,N'Thôn Tây'),
+('TN004',N'Vũ Minh',N'Đào',04,N'Thôn Nam'),
+('TN005',N'Phạm Thị',N'Hồng',05,N'Thôn Bắc')
+
+insert into PhanLoai values
+('KTNN56',N'Kỹ thuật nuôi vit cỏ'),
+('VHTD09',N'Địa lý thế giới'),
+('CNTT23',N'Khoa học máy tính cơ bản'),
+('VHTD13',N'Lịch sử thế giới hiện đại'),
+('VHTD17',N'Văn học Việt Nam hiện đại')
+
+insert into NXB values 
+('GD00',N'NXB Giáo dục'),
+('KHKT',N'NXB Khoa học kỹ thuật'),
+('XH00', N'NXB Xã hội'),
+('VH00', N'NXB Văn hóa'),
+('QT00', N'NXB Quốc tế')
+
+insert into Sach values
+('NN0024',N'Kỹ thuật nuôi vit cỏ','KTNN56','KHKT'),
+('TS0035', N'Địa lý thế giới', 'VHTD09', 'GD00'),
+('CN0042', N'Khoa học máy tính cơ bản', 'CNTT23', 'KHKT'),
+('LS0057', N'Lịch sử thế giới hiện đại', 'VHTD13', 'XH00'),
+('VH0065', N'Văn học Việt Nam hiện đại', 'VHTD17', 'VH00')
+
+insert into ChiTietMS values
+('TN001', 'VH0065', '2021-03-12', '2021-03-15'),
+('TN002', 'NN0024', '2021-11-07', null),
+('TN003', 'LS0057', '2021-03-27', null),
+('TN004', 'TS0035', '2021-05-07', '2021-05-10'),
+('TN005', 'CN0042', '2021-07-17', '2021-07-25')
+
+select * from NguoiMuon
+select * from ChiTietMS
+select * from Sach
+select * from PhanLoai
+select * from NXB
+
+--Câu 2
+--a. Liệt kê những cuốn sách người mượn trước ngày 1/4/2021 nhưng chưa trả (Mã sách, tên sách, họ tên người mượn, ngày mượn, số ngày quá hạn)
+select s.MSSACH, s.TenSach, nm.HoLotNM+' '+nm.TenNM as 'Ngươi mượn', ct.NgayMuon, datediff(day,ct.NgayMuon,getdate()) as 'Số ngày quá hạn'
+from Sach s inner join ChiTietMS ct on ct.MSSACH = s.MSSACH
+inner join NguoiMuon nm on nm.MSNM = ct.MSNM
+where ct.NgayMuon < '2021-04-01' and ct.NgayTra is null
+group by s.MSSACH, s.TenSach,nm.HoLotNM+' '+nm.TenNM, ct.NgayMuon
+
+--b. Đếm số người mượn theo từng thôn của từng quý năm 2021
+select nm.Thon as 'Thôn', 
+case
+when month(NgayMuon) between 1 and 3 then 'Quý 1'
+when month(NgayMuon) between 4 and 6 then 'Quý 2'
+when month(NgayMuon) between 7 and 9 then 'Quý 3'
+else 'Quý 4'
+end as 'Quý',
+count(nm.MSNM) as 'Số lượng'
+from NguoiMuon nm inner join ChiTietMS ct on ct.MSNM = nm.MSNM
+where year(ct.NgayMuon) = 2021
+group by nm.Thon,
+case
+when month(NgayMuon) between 1 and 3 then 'Quý 1'
+when month(NgayMuon) between 4 and 6 then 'Quý 2'
+when month(NgayMuon) between 7 and 9 then 'Quý 3'
+else 'Quý 4'
+end
+
+--c. Cho biết tên 2 phân loại có số lượng sách nhiều nhất
+select top 2 pl.ThongTinPL as 'Thông tin', count(s.MSSACH) as 'Số lượng sách'
+from PhanLoai pl inner join Sach s on s.MPL = pl.MPL
+group by pl.ThongTinPL
+order by count(s.MSSACH) desc
+
+--d. Tính tiền phạt cho các cuốn sách mượn quá 20 ngày, biết mỗi ngày phạt 1000 đồng
+select nm.TenNM as 'Người mượn', s.TenSach as 'Sách', datediff(day,ct.NgayMuon,getdate()) - 20 as 'Số ngày trễ', 
+case
+when datediff(day,ct.NgayMuon,getdate()) > 20 then (datediff(day,ct.NgayMuon,getdate()) - 20) * 1.000
+else 0
+end as 'Tiền phạt'
+from Sach s inner join ChiTietMS ct on ct.MSSACH = s.MSSACH
+inner join NguoiMuon nm on nm.MSNM = ct.MSNM
+where ct.NgayTra is null and datediff(day,ct.NgayMuon,getdate()) > 20
+
+--e. Liệt kê tên các nhà xuất bản có 10 (2) đầu sách trở lên
+select n.TenNXB as 'Nhà xuất bản', count(s.MSSACH) as 'Số đầu sách'
+from NXB n inner join Sach s on s.MSNXB = n.MSNXB
+group by n.TenNXB
+having count(s.MSSACH) >= 2
+
+--f. Liệt kê tên những người mượn có mượn tất cả các sách của thư viện
+select nm.HoLotNM+' '+nm.TenNM as 'Người mượn' from NguoiMuon nm
+where not exists (select * from Sach s where not exists (select * from ChiTietMS ct where ct.MSNM = nm.MSNM and s.MSSACH = ct.MSSACH))
+
+--Câu 3
+--a. Tạo thủ tục cho phép chèn dữ liệu vào bảng ChiTietMS
+create proc Chen_DL
+@MSNM varchar(5),
+@MSSACH varchar(10),
+@NgayMuon date,
+@NgayTra date
+as begin 
+insert into ChiTietMS (MSNM,MSSACH,NgayMuon,NgayTra)
+values (@MSNM,@MSSACH,@NgayMuon,@NgayTra) end
+
+-- Minh họa sử dụng thủ tục
+exec Chen_DL 'TN003', 'VH0065', '2021-12-10', '2021-12-17'
+
+--b. Hàm tạo trả về một bảng thống kê thông tin của tất cả các sách và tổng số lần được mượn của mỗi sách
+--(Mã sách, tên sách, tên nhà xuất bản, tổng số lần mượn)
+create function ThongKe() 
+returns table
+as return (
+select s.MSSACH as 'Mã sách', s.TenSach as 'Sách', n.TenNXB as 'Nhà Xuất Bản', count(ct.MSNM) as 'Số lần mượn'
+from ChiTietMS ct inner join Sach s on ct.MSSACH = s.MSSACH 
+inner join NXB n on n.MSNXB = s.MSNXB
+group by s.MSSACH , s.TenSach , n.TenNXB
+)
+
+--Minh họa sử dụng hàm
+select * from ThongKe();
